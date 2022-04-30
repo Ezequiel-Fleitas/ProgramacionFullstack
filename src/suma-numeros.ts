ccfffba7e1b8c467cc876d43527579da1752fee2
Ezequiel-Fleitas <104237053+Ezequiel-Fleitas@users.noreply.github.com>
2022-04-29 22:23:01 -0300

    suma entre intervalo de numeros

---
+++ src/suma-numeros.ts
@@ -0,0 +1,14 @@
+let primerNumero: number = Number(prompt("ingrese el primer numero"));
+let segundoNumero: number = Number(prompt("ingrese el segundo numero"));
+let aux, suma: number;
+suma = aux = 0;
+
+if (primerNumero > segundoNumero) {
+  aux = primerNumero;
+  primerNumero = segundoNumero;
+  segundoNumero = aux;
+}
+for (let i: number = primerNumero; i <= segundoNumero; i++) {
+  suma = suma + i;
+  console.log("resultado:", suma);
+}
